d6bf001148ba4693075b357ec25c0d7de90fa4b2
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-17 18:22:44 +0530

    created a userSchema,and create userModel and succefully aded user collection in data used to postman

--- Server/Controller/controller.js
+++ Server/Controller/controller.js
@@ -1,5 +1,24 @@
-async function registerPost(req,res) {
-    console.log(req.body)
+const userModel = require("../Models/usersModel")
+
+async function registerPost(req, res) {
+    try {
+        const newData = new userModel(req.body)
+        const saved =await newData.save()
+        res.json({success:true,data:saved})
+    } catch (error) {
+        console.log("the eror is"+error)
+        if(error.code===11000){
+            res.status(400).json({
+                success:false,
+                message:"Email already exists, please use another email"
+            })
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Internal server error."
+            });
+        }
+    }
 }
 
-module.exports={registerPost}
+module.exports = { registerPost }
---
+++ Server/Models/usersModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default:"user"
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+const userModel=mongoose.model("user",userSchema)
+
+module.exports=userModel
