c05b11d95e0efd185c548e52a924be940afab04f
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-26 11:26:43 +0530

    the getAllUser admin api ready

--- Server/Controller/adminContorller.js
+++ Server/Controller/adminContorller.js
@@ -10,9 +10,15 @@ const orderModel = require("../Models/ordersModel")
 require('dotenv').config()
 
 
-async function getAllUser(req,res){
-    const userData=await userModel.find()
-    res.json({userData})
+async function getAllUser(req, res) {
+    try {
+        const userData = await userModel.find()
+        res.status(200).json({success:true, data: userData })
+    } catch (error) {
+        res.status(200).json({success:false,message:"internal server error"})
+    }
 }
 
-module.exports = {getAllUser}
+
+
+module.exports = { getAllUser }
