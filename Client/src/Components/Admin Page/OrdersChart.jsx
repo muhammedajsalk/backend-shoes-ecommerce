e2f298c80097ef3e53d2c934cda451026773e651
muhammedajsalk <ajsalmuhammed747@gmail.com>
2025-05-29 14:20:29 +0530

    the order section in admin api is seted

--- Client/src/Components/Admin Page/OrdersChart.jsx
+++ Client/src/Components/Admin Page/OrdersChart.jsx
@@ -7,22 +7,23 @@ function OrdersChart() {
   const [fetchData, setFetchData] = useState([]);
   useEffect(() => {
     axios
-      .get("https://shoes-ecommerce-9ems.onrender.com/users")
-      .then((response) => setFetchData(response.data))
+      .get("http://localhost:5000/api/admin/orders",{withCredentials:true})
+      .then((response) => {
+        setFetchData(response.data.data)
+        console.log(response.data.data)
+      })
       .catch((err) => toast.error("users data fetch error found",err));
   }, []);
 
-  const filteredOrders = fetchData.flatMap(user => user.orders);
-  const onefilteredorders =useMemo(()=>filteredOrders.filter(items => items.payment_total),[fetchData])
-  const revenueCal=onefilteredorders.reduce((sum,items)=>{
-        return sum+=Number(items.payment_total)
+  const revenueCal=fetchData.reduce((sum,items)=>{
+        return sum+=items.total
   },0)
   
   return (
     <div className="flex flex-wrap justify-center gap-6 p-4">
       <div className="max-w-[400px] w-full md:w-[300px] lg:w-[350px] bg-white shadow-lg rounded-2xl border border-gray-200 flex flex-col items-center justify-center gap-4 p-6">
         <h1 className="text-2xl md:text-3xl font-semibold text-gray-800">total Orders</h1>
-        <h2 className="text-5xl font-bold text-blue-600">{onefilteredorders.length}</h2>
+        <h2 className="text-5xl font-bold text-blue-600">{fetchData.length}</h2>
       </div>
 
       <div className="max-w-[400px] w-full md:w-[300px] lg:w-[350px] bg-white shadow-lg rounded-2xl border border-gray-200 flex flex-col items-center justify-center gap-4 p-6">
